b75ea6af28956e8d7f89c1b05fc25cdb6e75e353
strahil9810 <strahil1998@abv.bg>
2023-01-06 16:25:22 +0200

    Task 4

--- Balls.js
+++ Balls.js
@@ -37,7 +37,7 @@ function balls(input) {
             other++;
         }
     }
-    console.log(`Total points: ${point}`);
+    console.log(`Total points: ${Math.floor(point)}`);
     console.log(`Red balls: ${red}`);
     console.log(`Orange balls: ${orange}`);
     console.log(`Yellow balls: ${yellow}`);
@@ -45,4 +45,4 @@ function balls(input) {
     console.log(`Other colors picked: ${other}`);
     console.log(`Divides from black balls: ${black}`);
 }
-balls([3, "white", "black", "pink"])
+balls([10, "white", "white", "ee", "red", "orange", "red", "black", "black", "black", "black"])
